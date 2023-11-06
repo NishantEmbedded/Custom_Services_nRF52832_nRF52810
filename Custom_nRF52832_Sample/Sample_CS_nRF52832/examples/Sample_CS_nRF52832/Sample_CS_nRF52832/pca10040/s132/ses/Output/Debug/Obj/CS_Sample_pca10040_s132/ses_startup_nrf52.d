b2ecf645f23a4f947658cc4741f08b4eaea82461
Nishant Parmar <nishant.parmarforgithub@gmail.com>
2023-11-06 16:06:35 +0530

    Cusotm_Service_nRF52832
    In this project have a custom service created on nRF52832 custom board.
    1)Write Service
    2)Notify Service

---
+++ Custom_nRF52832_Sample/Sample_CS_nRF52832/examples/Sample_CS_nRF52832/Sample_CS_nRF52832/pca10040/s132/ses/Output/Debug/Obj/CS_Sample_pca10040_s132/ses_startup_nrf52.d
@@ -0,0 +1,2 @@
+Output/Debug/Obj/CS_Sample_pca10040_s132/ses_startup_nrf52.o: \
+ D:\Nishant\GIT_DESK\Custom_Services_nRF52832_nRF52810\Custom_nRF52832_Sample\Sample_CS_nRF52832\modules\nrfx\mdk\ses_startup_nrf52.s
---
+++ Custom_nRF52832_Sample/Sample_CS_nRF52832/examples/Sample_CS_nRF52832/Sample_CS_nRF52832/pca10040/s132/ses/Output/Debug/Obj/CS_Sample_pca10040_s132/utf.d
@@ -0,0 +1,9 @@
+Output/Debug/Obj/CS_Sample_pca10040_s132/utf.o: \
+ D:\Nishant\GIT_DESK\Custom_Services_nRF52832_nRF52810\Custom_nRF52832_Sample\Sample_CS_nRF52832\external\utf_converter\utf.c \
+ D:\Nishant\GIT_DESK\Custom_Services_nRF52832_nRF52810\Custom_nRF52832_Sample\Sample_CS_nRF52832\external\utf_converter\utf.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 7.10a/include/stddef.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 7.10a/include/__SEGGER_RTL.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 7.10a/include/__SEGGER_RTL_ConfDefaults.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 7.10a/include/__SEGGER_RTL_Conf.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 7.10a/include/__SEGGER_RTL_Arm_Conf.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 7.10a/include/stdint.h
